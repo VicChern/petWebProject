22bf4ab93ea19f1d76e49c681522d29ca5126df1
Vicchern <itstud87@gmail.com>
2019-02-06 00:28:39 +0200

    Lets our buttons to scroll to special sections on the page

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    
+    /*For the sticky navigation*/
     $('.js--section-features').waypoint(function(direction) {
         if (direction == "down") {
             $('nav').addClass('sticky');
@@ -9,6 +11,15 @@ $(document).ready(function () {
         offset:'60px;'
     
     });
+    
+    //scroll buttons
 
+   $('.js--scroll-to-plans').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000); 
+    });
+    
+    $('.js--scroll-to-start').click(function () {
+       $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000); 
+    }); 
 });
 
